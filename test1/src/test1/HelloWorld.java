a880ffcf06a11e33a1841abb282033b886b88c49
hds1219 <1571710215@qq.com>
2020-04-13 23:38:13 +0800

    v3.0

--- test1/src/test1/HelloWorld.java
+++ test1/src/test1/HelloWorld.java
@@ -3,6 +3,8 @@ package test1;
 public class HelloWorld {
 	public static void main(String[] args) {
 		String str="helloJava";
+		int a=10;
+		System.out.println(a);
 		System.out.println(str);
 		System.out.println("HelloWorld");
 	}
